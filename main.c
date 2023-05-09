c2fd04af290e21d999f845ccbe33f454331b0fff
Fanat0x <117281041+Fanat0x@users.noreply.github.com>
2023-05-09 16:31:54 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+//Import des différentes librairies C nécessaires
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+//Définition de la taille maximale du tableau
+
+#define gridMaxX 9
+#define gridMaxY 9
+#define bloc 9
+
